312a3e5f50e87a9961307e7153792f38a8bdbe79
Jason Stokes <jpstokes@plecco.net>
2015-06-02 16:22:28 -0400

    create routes and add test and controller action for getting user

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,2 +1,16 @@
 class LeaderboardController < ApplicationController
+  def index
+    leaderboard = Leaderboard.find_by_name(params[:name])
+    if leaderboard
+      render json: leaderboard
+    else
+      render nothing: true, status: 404
+    end
+  end
+
+  private
+
+  def leaderboard_params
+    params.require(:leaderbaord).permit [:name, :score]
+  end
 end
--- spec/controllers/leaderboard_controller_spec.rb
+++ spec/controllers/leaderboard_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe LeaderboardController, :type => :controller do
 
+  describe '#index' do
+    it 'return the score and rank of the user' do
+      Leaderboard.create(name: 'John Doe', score: 20)
+      get :index, name: 'John Doe'
+      result = JSON.parse(response.body)
+      expect(result['score']).to eq 20
+    end
+
+    it 'returns 404 if user is not found' do
+      get :index, name: 'Jane Doe'
+      expect(response.status).to eq 404
+    end
+  end
 end
