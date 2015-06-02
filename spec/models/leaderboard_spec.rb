7959186434fff05feef1f498c57ece99e561551c
Jason Stokes <jpstokes@plecco.net>
2015-06-02 14:57:08 -0400

    create model and test

---
+++ spec/models/leaderboard_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Leaderboard, :type => :model do
+
+  let(:leaderboard) { Leaderboard.new }
+
+  subject { leaderboard }
+
+  it { should respond_to :name }
+  it { should respond_to :score }
+end
